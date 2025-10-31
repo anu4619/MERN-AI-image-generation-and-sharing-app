6f9024efa0534cc568dc2048c51e89734fbbbe94
anu4619 <anushka4619@gmail.com>
2025-10-31 21:43:16 +0530

    getting started on backend

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', async (requestAnimationFrame,res) =>{
+    res.send('hello from DALL-E!');
+})
+
+const startServer = async () => {
+
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(5000,()=> console.log('Server has started on port http://localhost:5000'))
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+    
+}
+
+startServer();
